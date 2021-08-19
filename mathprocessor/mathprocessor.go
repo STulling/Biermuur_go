4e994064e7206231a2575a1ece6c14c38a40f7e0
Simon Tulling <simon.tulling99@gmail.com>
2021-08-19 14:40:13 +0200

    debug stuff

--- mathprocessor/mathprocessor.go
+++ mathprocessor/mathprocessor.go
@@ -2,20 +2,22 @@ package mathprocessor
 
 import (
 	"fmt"
-	"github.com/STulling/Biermuur_go/displaycontroller"
 	"time"
+
+	"github.com/STulling/Biermuur_go/displaycontroller"
 )
 
 var (
 	ToCalculate = make(chan []byte, 0)
-	prevTime = time.Now()
+	prevTime    = time.Now()
 )
 
 func RunCalculationPipe() {
 	for {
-		<-ToCalculate
+		data := <-ToCalculate
 		fmt.Println(time.Since(prevTime))
 		prevTime = time.Now()
+		print(len(data))
 		//rms, tone := float64(data[0]), 0. //processing.ProcessBlock(data)
 		displaycontroller.ToDisplay <- [2]float64{0.8, 0}
 	}
