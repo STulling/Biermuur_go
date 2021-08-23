898d4f258cb8714137d354c86de29267d3140ce6
Simon Tulling <simon.tulling99@gmail.com>
2021-08-23 21:32:20 +0200

    remove small slowdown

--- mathprocessor/mathprocessor.go
+++ mathprocessor/mathprocessor.go
@@ -1,7 +1,6 @@
 package mathprocessor
 
 import (
-	"fmt"
 	"github.com/STulling/Biermuur_go/audio/processing"
 	"github.com/STulling/Biermuur_go/displaycontroller"
 	"github.com/STulling/Biermuur_go/globals"
@@ -14,16 +13,18 @@ var (
 	// the pipeline is keeping up.
 	// I just have this buffer if the timer is acting up
 	ToCalculate = make(chan [][2]float64, 64)
-	NewStuff = make(chan bool, 1)
+	NewStuff = make(chan bool, 10)
 	prevTime    = time.Now()
 )
 
 func RunCalculationPipe(sampleRate int) {
 	for {
 		<-NewStuff
-		fmt.Println(len(NewStuff))
 		ticker := time.NewTicker(time.Second / time.Duration(sampleRate/globals.BLOCKSIZE))
-		cycles := 0
+		data := <-ToCalculate
+		rms, tone := processing.ProcessBlock(data)
+		displaycontroller.ToDisplay <- [2]float64{rms, tone}
+		cycles := 1
 		for cycles < 2*globals.AUDIOSYNC {
 			<-ticker.C
 			data := <-ToCalculate
